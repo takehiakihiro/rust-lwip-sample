155b7ce91d8e08ded7dcd1d5e8c68d4f1da8d6c2
takehiakihiro <akihiro2@gmail.com>
2024-09-22 11:59:23 +0900

    とりあえずTUNとacceptだけできるようになった

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use anyhow::Result;
 use clap::Parser;
 use futures::{SinkExt, StreamExt};
-use std::net::{IpAddr, SocketAddr};
+use lwip::NetStack;
+use std::{
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+    os::fd::AsRawFd,
+};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio_tun::Tun;
+// use udp_stream::UdpStream;
 
-const MTU: u16 = 1400;
+// const MTU: u16 = 1400;
 
 #[repr(C)]
 #[derive(Default, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, clap::ValueEnum)]
@@ -20,22 +27,13 @@ pub enum ArgVerbosity {
 #[derive(Parser)]
 #[command(author, version, about = "Testing app for tun.", long_about = None)]
 struct Args {
-    /// echo server address, likes `127.0.0.1:4000`
-    #[arg(short, long, value_name = "IP:port", default_value = "127.0.0.1:4000")]
-    vpn_addr: SocketAddr,
+    // /// echo server address, likes `127.0.0.1:4000`
+    // #[arg(short, long, value_name = "IP:port", default_value = "127.0.0.1:4000")]
+    // vpn_addr: SocketAddr,
 
-    /// echo server address, likes `127.0.0.1`
-    #[arg(short, long, value_name = "IP address", default_value = "127.0.0.1")]
-    server_addr: IpAddr,
-
-    /// tcp timeout
-    #[arg(long, value_name = "seconds", default_value = "10")]
-    tcp_timeout: u64,
-
-    /// udp timeout
-    #[arg(long, value_name = "seconds", default_value = "10")]
-    udp_timeout: u64,
-
+    // /// echo server address, likes `127.0.0.1`
+    // #[arg(short, long, value_name = "IP address", default_value = "127.0.0.1")]
+    // server_addr: IpAddr,
     /// Verbosity level
     #[arg(short, long, value_name = "level", value_enum, default_value = "info")]
     pub verbosity: ArgVerbosity,
@@ -48,41 +46,50 @@ async fn main() -> Result<()> {
     let default = format!("{:?}", args.verbosity);
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or(default)).init();
 
-    let mut ipstack_config = IpStackConfig::default();
-    ipstack_config.mtu = MTU;
-    ipstack_config.tcp_timeout = std::time::Duration::from_secs(args.tcp_timeout);
-    ipstack_config.udp_timeout = std::time::Duration::from_secs(args.udp_timeout);
+    // TUNデバイスの作成
+    let tun = Tun::builder()
+        .address(Ipv4Addr::new(10, 0, 0, 1))
+        .netmask(Ipv4Addr::new(255, 255, 255, 0))
+        .up() // or set it up manually using `sudo ip link set <tun-name> up`.
+        .try_build() // or `.try_build_mq(queues)` for multi-queue support.
+        .unwrap();
 
-    let mut vpn_stream = UdpStream::connect(args.vpn_addr).await?;
-    let buf: [u8; 1] = [0];
-    vpn_stream.write_all(&buf).await?;
+    println!("tun created, name: {}, fd: {}", tun.name(), tun.as_raw_fd());
 
-    let (stack, mut tcp_listener, udp_socket) = ::lwip::NetStack::new().unwrap();
+    let (mut tun_reader, mut tun_writer) = tokio::io::split(tun);
+
+    // let mut vpn_stream = UdpStream::connect(args.vpn_addr).await?;
+    // let buf: [u8; 1] = [0];
+    // vpn_stream.write_all(&buf).await?;
+
+    let (stack, mut tcp_listener, udp_socket) = NetStack::new()?;
     let (mut stack_sink, mut stack_stream) = stack.split();
 
-    // tun device is assumed implementing `Stream` and `Sink`
-    let (mut tun_sink, mut tun_stream) = tun.split();
-
     // Reads packet from stack and sends to TUN.
-    tokio::spawn(async move {
+    let stack_reader_task = tokio::spawn(async move {
         while let Some(pkt) = stack_stream.next().await {
             if let Ok(pkt) = pkt {
-                tun_sink.send(pkt).await.unwrap();
+                tun_writer.write(&pkt).await.unwrap();
             }
         }
     });
 
     // Reads packet from TUN and sends to stack.
-    tokio::spawn(async move {
-        while let Some(pkt) = tun_stream.next().await {
-            if let Ok(pkt) = pkt {
-                stack_sink.send(pkt).await.unwrap();
-            }
+    let stack_writer_task = tokio::spawn(async move {
+        loop {
+            let mut pkt = [0u8; 4096];
+            let n = match tun_reader.read(&mut pkt).await {
+                Ok(v) => v,
+                Err(e) => {
+                    return;
+                }
+            };
+            stack_sink.send(pkt[..n].to_vec()).await.unwrap();
         }
     });
 
     // Extracts TCP connections from stack and sends them to the dispatcher.
-    tokio::spawn(async move {
+    let tcp_listener_task = tokio::spawn(async move {
         while let Some((stream, local_addr, remote_addr)) = tcp_listener.next().await {
             tokio::spawn(handle_inbound_stream(stream, local_addr, remote_addr));
         }
@@ -90,9 +97,38 @@ async fn main() -> Result<()> {
 
     // Receive and send UDP packets between netstack and NAT manager. The NAT
     // manager would maintain UDP sessions and send them to the dispatcher.
-    tokio::spawn(async move {
-        handle_inbound_datagram(udp_socket).await;
-    });
+    let udp_listener_task = tokio::spawn(async move { handle_inbound_datagram(udp_socket).await });
+
+    let _ = tokio::join!(
+        stack_reader_task,
+        stack_writer_task,
+        tcp_listener_task,
+        udp_listener_task
+    );
+
+    Ok(())
+}
+
+///
+async fn handle_inbound_stream(
+    stream: lwip::TcpStream,
+    local_addr: SocketAddr,
+    remote_addr: SocketAddr,
+) -> Result<()> {
+    log::info!("accepted tcp {} -> {}", local_addr, remote_addr);
+    Ok(())
+}
+
+///
+async fn handle_inbound_datagram(mut udp_socket: Box<lwip::UdpSocket>) -> Result<()> {
+    while let Some((pkt, local_addr, remote_addr)) = udp_socket.next().await {
+        log::info!(
+            "recved udp {} -> {}, len={}",
+            local_addr,
+            remote_addr,
+            pkt.len()
+        );
+    }
 
     Ok(())
 }
